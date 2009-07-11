66371f2e4127d510853f66a08ee8f972b1b35c8e
Roy Wright <roy@wright.org>
2009-07-11 02:31:57 -0500

    added gem version checking

--- lib/drbman.rb
+++ lib/drbman.rb
@@ -2,6 +2,15 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems'
+
+gem 'ruby-debug',       '0.10.3'
+gem 'log4r',            '1.0.5'
+gem 'user-choices',     '1.1.6'
+gem 'extlib',           '0.9.12'
+gem 'versionomy',       '0.0.4'
+gem 'net-ssh',          '2.0.11'
+gem 'net-scp',          '1.0.2'
+
 require 'extlib'
 require 'ruby-debug'
 require 'versionomy'
