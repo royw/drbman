f9042de0c3822d313b9074a0aec4a8abe6a58011
Roy Wright <roy@wright.org>
2009-07-14 15:34:05 -0500

    specs still broke

--- examples/primes/spec/primes_spec.rb
+++ examples/primes/spec/primes_spec.rb
@@ -1,20 +1,24 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'spec'
-require 'sieve_of_eratosthenes'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'primes'
 
 
 describe('SieveOfEratosthenes') do
+  before(:each) do
+    @choices = {}
+    @logger = Log4r::Logger.new('primes_spec')
+    @logger.level = Log4r::DEBUG
+  end
+  
   it 'should find [2,3,5,7,11,13,17,19] for n=20' do
-    sieve = SieveOfEratosthenes.new(20)
+    @choices[:max_integer] = 20
+    sieve = Primes.new(@logger, @choices)
     sieve.execute.should == [2,3,5,7,11,13,17,19]
   end
   it 'should take a while' do
-    sieve = SieveOfEratosthenes.new(10000000)
+    sieve = SieveOfEratosthenes.new(10000000, @choices, @logger)
     primes = sieve.execute
     puts "#{primes.length} primes found"
     primes.length.should == 664579
   end
 end
 
-# 1111 1111 1111 1111 0000
